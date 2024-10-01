ecf86d21c4f36134fe311b345e2b30b8d6813b57
Rick Liu <rickliu000@gmail.com>
2024-09-30 21:35:58 -0700

    Fixed bug where it would only draw when hovering, now works when holding down mouse

--- script.js
+++ script.js
@@ -2,9 +2,26 @@ const canvas = document.querySelector(".canvas");
 const submit = document.querySelector(".submit");
 const grid_size = document.querySelector(".grid_size");
 const clear = document.querySelector(".clear");
+const color_picker = document.querySelector(".color_picker")
+const randomize = document.querySelector(".random")
+const erase = document.querySelector(".erase")
+
+let color = "black"
+let current_grid_size = 16
+let eraser = false;
+
+let isMouseDown = false;
+
+document.addEventListener('mousedown', () => {
+  isMouseDown = true;
+});
+
+document.addEventListener('mouseup', () => {
+  isMouseDown = false;
+});
 
 document.addEventListener('DOMContentLoaded', () => {
-    change_grid(16)
+    change_grid(current_grid_size)
 }, false);
 
 function change_grid(pixels) {
@@ -13,12 +30,13 @@ function change_grid(pixels) {
         const pixel = document.createElement("div");
         pixel.classList.add("grid");
         canvas.appendChild(pixel);
-        pixel.style.background = "white";
         const rel = (canvas.clientWidth / pixels).toString();
         pixel.style.width = rel + "px";
         pixel.style.height = rel + "px";
         pixel.addEventListener("mouseover", () =>{
-            pixel.style.background = "black";
+            if (isMouseDown){
+                pixel.style.background = color;
+            }
         })
         clear.addEventListener("click", () =>{
             pixel.style.background = "white";
@@ -26,10 +44,40 @@ function change_grid(pixels) {
     };
 }
 
+color_picker.addEventListener("change", () =>{
+    color = color_picker.value;
+})
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
 submit.addEventListener("click", () =>{
-    change_grid(grid_size.value);
-    grid_size.value = ""
+    current_grid_size = grid_size.value;
+    change_grid(current_grid_size);
+    grid_size.value = "";
 });
 
+randomize.addEventListener("click", () =>{
+    color = getRandomColor();
+    color_picker.value = color;
+});
+
+erase.addEventListener("click", (e) =>{
+    if (eraser){
+        e.target.style.background = ""
+        color = color_picker.value;
+        eraser = false
+    }
+    else{
+        eraser = true;
+        color = "white";
+        e.target.style.background = "rgb(140, 140, 140)";
+    }
+});
+    
